b40227276dbd51eb5c84aa6aec366d5525abc510
lee <leelf_00@126.com>
2023-11-29 08:23:23 +0800

    Update 2023_11_23_world_00.sql

--- sql/updates/master/world/2023_11_23_world_00.sql
+++ sql/updates/master/world/2023_11_23_world_00.sql
@@ -1,4 +1,5 @@
-DELETE FROM `creature_text` WHERE `entry` IN (54567, 54943);
-INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`) VALUES
-(54567, 'A challenge in compassion and humility. By helping Merchant Lorvo, it would seem you\'ve passed, $c. Master Shang would be pleased.', 12, 100),
-(54943, 'Shhhh! Come quietly. She\'s practicing.', 12, 100);
+-- The creature texts already in db,no need to apply this sql
+--DELETE FROM `creature_text` WHERE `entry` IN (54567, 54943);
+--INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`) VALUES
+--(54567, 'A challenge in compassion and humility. By helping Merchant Lorvo, it would seem you\'ve passed, $c. Master Shang would be pleased.', 12, 100),
+--(54943, 'Shhhh! Come quietly. She\'s practicing.', 12, 100);
