dde32d485d03a74b15529753eee13437fb6b380c
lee <leelf_00@126.com>
2023-11-29 08:30:20 +0800

    Update 2023_11_16_world_03.sql

--- sql/updates/master/world/2023_11_16_world_03.sql
+++ sql/updates/master/world/2023_11_16_world_03.sql
@@ -1,18 +1,19 @@
-DELETE FROM `creature_addon` WHERE `guid`=224522;
-INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
-(224522, '80797');
-
-DELETE FROM `creature_addon` WHERE `guid`=224519; 
-INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
-(224519, '80797');
-
-DELETE FROM `creature_addon` WHERE `guid`=224593;
-INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
-(224593, '82358');
-
-DELETE FROM `creature_addon` WHERE `guid`=224592; 
-INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
-(224592, '82358');
+-- Lorvo and aysa guid is wrong. In our db Aysa(entry 54567) has 3 guids 563511,563545,571938   , Lorvo(entry 54943) has 2 guids 563517,571937.You must check the quest in game which guids should apply these auras.
+-- DELETE FROM `creature_addon` WHERE `guid`=224522;
+-- INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
+-- (224522, '80797');
+--
+-- DELETE FROM `creature_addon` WHERE `guid`=224519; 
+-- INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
+-- (224519, '80797');
+--
+-- DELETE FROM `creature_addon` WHERE `guid`=224593;
+-- INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
+-- (224593, '82358');
+--
+-- DELETE FROM `creature_addon` WHERE `guid`=224592; 
+-- INSERT INTO `creature_addon` (`guid`, `auras`) VALUES
+--(224592, '82358');
 
 DELETE FROM `spell_area` WHERE `spell`=102393;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
